f6d12f24b8bfbcd404cd3e7950de286c3f1c1486
szx21023 <szx21023@gmail.com>
2025-05-20 11:43:33 +0800

    web: 3_routing

---
+++ web/3_routing.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You'v requested the book: %s on page: %s", title, page)
+	})
+
+	http.ListenAndServe(":8080", r)
+}
