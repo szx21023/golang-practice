62c4c98caf86e22d75eb8502dfa697b9c4738f8e
szx21023 <szx21023@gmail.com>
2025-07-09 11:11:36 +0800

    feat: add GenerateToken function

--- gin/internal_module/user/service.go
+++ gin/internal_module/user/service.go
@@ -1,7 +1,12 @@
 package user
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+    "github.com/golang-jwt/jwt/v5"
+)
 
+var jwtSecret = []byte("your_secret_key") // JWT 密鑰
 // 模擬的資料庫
 var users = map[string]User{}
 
@@ -34,4 +39,14 @@ func CreateUser(req CreateUserRequest) User {
     }
     users[id] = user
     return user
+}
+
+func GenerateToken(userID string) (string, error) {
+    claims := jwt.MapClaims{
+        "user_id": userID,
+        "exp": time.Now().Add(time.Hour * 72).Unix(),
+    }
+
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString(jwtSecret)
 }
